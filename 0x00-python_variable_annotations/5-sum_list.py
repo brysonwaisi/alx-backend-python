4e68124f0f99aa10c98a80e2c520f5fac70a9612
brysonwaisi <brysonnyamwange@gmail.com>
2023-01-05 23:15:56 +0300

    Complex types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 5 - complex types
+'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''sums list of floating point numbers'''
+    return float(sum(input_list))
